a60e2ea9fea463e9e214e28b418968e482335783
PS216587 <PS216587@edu.summacollege.nl>
2023-05-24 22:19:10 +0200

    commit

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -10,8 +10,8 @@
         : 'text-gray-600 hover:text-black';
 
     $classes = ($active ?? false)
-        ? 'inline-flex items-center px-1 pt-1 text-2xl leading-5 focus:outline-none transition font-extrabold mb-0.5 '.$index1
-        : 'inline-flex items-center px-1 pt-1 text-md hover:scale-110 font-semibold leading-5 hover:border-gray-300 transition duration-300 ease-in-out hidden sm:flex mb-0.5 '.$index2;
+        ? 'not-italic inline-flex items-center px-1 pt-1 text-2xl leading-5 focus:outline-none transition mb-0.5 '.$index1
+        : 'not-italic inline-flex items-center px-1 pt-1 text-md hover:scale-110 font-semibold leading-5 hover:border-gray-300 transition duration-300 ease-in-out hidden sm:flex mb-0.5 '.$index2;
 @endphp
 
 <a {{ $attributes->merge(['class' => $classes]) }}>
